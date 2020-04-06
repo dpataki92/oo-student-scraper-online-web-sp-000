eff9726072daee14d4336d85b9936ff0a0ad508d
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-06 23:16:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,13 +8,11 @@ class Scraper
 
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    students = []
-    doc.css(".student-card").each do |hash|
+    students = doc.css(".student-card").collect do |hash|
       student = {}
       student[:name] = doc.css(".card-text-container h4")[0].text
       student[:location] = doc.css(".card-text-container p")[0].text
       student[:profile_url] = doc.css(".student-card a")[0]["href"]
-      students << student
     end
     students
     binding.pry
