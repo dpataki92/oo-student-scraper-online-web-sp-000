dee5f4187e3954b796b78278d71d1ab678f13ef2
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-04-07 00:09:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,36 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    social = doc.css(".social-icon-container a")
+    twitter = nil
+    linkedin = nil
+    github = nil
+    blog = nil
+    profile_quote = doc.css(".vitals-text-container.profile-quote").text
+    bio = doc.css("div details-container.description-holder p").text
 
+    social.each {|el|
+    if el[0]["href"].include?("twitter")
+      twitter = el[0]["href"]
+    elsif el[0]["href"].include?("linkedin")
+      linkedin = el[0]["href"]
+    elsif el[0]["href"].include?("github")
+      github = el[0]["href"]
+    else
+      blog = el[0]["href"]
+    end
+    }
+
+    student = {}
+    student[:twitter] = twitter if twitter
+    student[:linkedin] = linkedin if linkedin
+    student[:github] = github if guthub
+    student[:blog] = blog if blog
+    student[:profile_quote] = profile_quote if profile_quote
+    student[:bio] = bio if bio
+    student
   end
 
 end
